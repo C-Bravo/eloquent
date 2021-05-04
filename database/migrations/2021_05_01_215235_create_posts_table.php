603637854da3c5f869377c589ce5cf47bd6f35ef
CarlosBravo <carlos.bravo@quind.io>
2021-05-04 11:02:25 -0500

    first commit

---
+++ database/migrations/2021_05_01_215235_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            //Relación con la tabla users
+            $table->unsignedBigInteger('user_id'); //Se crea un nuevo campo
+            $table->foreign('user_id')->references('id')->on('users'); //Se define la relación con el campo id de la tabla users 
+
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Post;
+
+
+Route::get('/', function () {
+    // $posts = Post::all(); 
+    $posts = Post::where('id', '>=', '20')
+        ->orderBy('id', 'desc')
+        ->take(3)
+        ->get();
+
+    foreach($posts as $post){
+        echo "$post->id $post->title <br>";
+    }
+});
+
+Route::get('/posts', function () {
+    // $posts = Post::all(); 
+    $posts = Post::get();
+
+    foreach($posts as $post){
+        echo "
+        $post->id
+        <strong>{$post->user->get_name}</strong> 
+        $post->get_title <br>";
+    }
+});
+
+use App\User;
+
+Route::get('/users', function () {
+    $users = User::get();
+
+    foreach($users as $user){
+        echo "
+        $user->id
+        $user->name
+        {$user->posts->count()} posts<br>";
+    }
+});
+
+Route::get('/collections', function () {
+    $users = User::get();
+
+    // dd($users);
+    // dd($users->contains(5));
+    // dd($users->except([1,2,3]));
+    // dd($users->only(4));
+    // dd($users->find(4));
+    dd($users->load('posts'));
+
+});
+
+Route::get('/serialization', function () {
+    $users = User::get();
+
+    // dd($users->toArray());
+    $user = $users->find(1);
+    // dd($user);
+    dd($user->toJson());
+
+});
